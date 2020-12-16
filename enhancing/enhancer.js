b5ff820f1bcb99ac69fed8c7f2a286daa79b3fed
Olivia Toolson <livytoolson@gmail.com>
2020-12-16 14:00:54 -0800

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,39 +2,57 @@ module.exports = {
   success,
   fail,
   repair,
-  get,
+  get
 }
 
 function success(item) {
-  return (
-    item.enhancement < 20 
-    ? { ...item, enhancment: item.enhancement + 1 } 
-    : { ...item }
-  )
-}
-
-function fail(item) {
-  if (item.enhancement < 15) {
+  const { enhancement } = item
+  if (enhancement >= 20) {
     return {
       ...item, 
-      durability: item.durability - 5
-    }
-  } else if (item.enhancement > 15) {
+      enhancement: enhancement
+    };
+  } else if (enhancement >= 0 && enhancement < 20) {
     return {
       ...item, 
-      durabilty: item.durability - 10
-    }
-  } else {
-    return {
-      ...item, 
-      durbaility: item.durability - 10, 
-      enhancement: item.enhancment - 1
-    }
+      enhancement: enhancement + 1
+    };
   }
 }
 
+function fail(item) {
+  const { enhancement, durability } = item;
+ if (enhancement >= 0 && enhancement < 15) {
+   return {
+     ...item, 
+     durability: durability - 5
+    }
+ } else if (enhancement >= 15 && enhancement <= 20) {
+   if (enhancement > 16) {
+     return {
+       ...item, 
+       enhancement: enhancement - 1, 
+       durability: durability - 10
+      }
+   } else {
+     return {
+       ...item, 
+       durability: durability - 10
+      }
+   }
+ } else {
+   return {...item}
+ }
+}
+
 function repair(item) {
-  return { ...item, durability: 100 };
+  const { enhancement, durability } = item
+  if (
+    durability <= 100 &&
+    durability >= 0 &&
+    enhancement >= 0 &&
+    enhancement <= 20
+    ) return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.test.js
+++ enhancing/enhancer.test.js
@@ -2,48 +2,48 @@ const { success, fail, repair } = require('./enhancer.js');
 
 const badItem = {
     name: 'Bad Item',
-    durability: 5, // 0 - 100
-    enhancemnet: 0 // 0 - 20
+    durability: 14, // 0 - 100
+    enhancement: 1 // 0 - 20
 }
 
 const goodItem = {
     name: 'Good Item',
     durability: 95,
-    enhancemnet: 20
+    enhancement: 20
 }
 
 describe('sanity check', () => {
-    it('should perform a sanity check', () => {
-        expect(true).toBe(true)
+    it('is working', () => {
+        expect(2 + 2).toBe(4)
     })
 })
 
-describe('success', () => {
+describe('success()', () => {
     it('should increase enhancement by 1', () => {
-        expect(success(badItem).enhancemnet).toBe(1)
+        expect(success(badItem).enhancement).toEqual(2)
     })
     it('should not increase enhancement if enhancement level is 20', () => {
-        expect(success(goodItem).enhancemnet).toBe(20)
+        expect(success(goodItem).enhancement).toBe(20)
     })
     it('should not change durability', () => {
         expect(success(goodItem).durability).toBe(95)
-        expect(success(badItem).durability).toBe(5)
+        expect(success(badItem).durability).toBe(14)
     }) 
 })
 
-describe('fail', () => {
+describe('fail()', () => {
     it('should decrease durability by 5 if enhancement level is less than 15', () => {
-        expect(fail(badItem).durability).toBe(0)
+        expect(fail(badItem).durability).toEqual(9)
     })
     it('should decrease durability by 10 if enhancement level is greater than 15', () => {
         expect(fail(goodItem).durability).toBe(85)
     })
     it('should decrease enhancement by 1 if enhancement level is greater than 16', () => {
-        expect(fail(goodItem).durability).toBe(19)
+        expect(fail(goodItem).enhancement).toBe(19)
     })
 })
 
-describe('repair', () => {
+describe('repair()', () => {
     it('should return an item with durability of 100', () => {
         expect(repair(badItem).durability).toBe(100)
     })
