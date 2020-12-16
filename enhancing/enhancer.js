52bf848d44ccf2b4785a11d7c5d903aff9ab16c8
Olivia Toolson <livytoolson@gmail.com>
2020-12-15 20:55:48 -0800

    test written, npm test not working in terminal

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -3,18 +3,38 @@ module.exports = {
   fail,
   repair,
   get,
-};
+}
 
 function success(item) {
-  return { ...item };
+  return (
+    item.enhancement < 20 
+    ? { ...item, enhancment: item.enhancement + 1 } 
+    : { ...item }
+  )
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return {
+      ...item, 
+      durability: item.durability - 5
+    }
+  } else if (item.enhancement > 15) {
+    return {
+      ...item, 
+      durabilty: item.durability - 10
+    }
+  } else {
+    return {
+      ...item, 
+      durbaility: item.durability - 10, 
+      enhancement: item.enhancment - 1
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
